9665cf9c94c890650184d1a7460699a2c1318c9e
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2024-04-22 16:00:31 -0700

    Bump ecs module to 2.7 that accepts AMI

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "kibana" {
-  source = "infrahouse/ecs/aws"
-  version = "= 2.6.1"
+  source  = "infrahouse/ecs/aws"
+  version = "= 2.7.0"
   providers = {
     aws     = aws
     aws.dns = aws.dns
@@ -8,6 +8,7 @@ module "kibana" {
   service_name                          = "${var.elasticsearch_cluster_name}-kibana"
   docker_image                          = "docker.elastic.co/kibana/kibana:8.12.0"
   load_balancer_subnets                 = var.load_balancer_subnets
+  ami_id                                = var.ami_id
   asg_subnets                           = var.asg_subnets
   zone_id                               = var.zone_id
   dns_names                             = ["${var.elasticsearch_cluster_name}-kibana"]
